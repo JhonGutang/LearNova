b935a42a55a7e897b4a8a875b3c8f713d2d76717
Jhon <jbgutang771@gmail.com>
2025-09-09 18:39:57 +0800

    modified the module schema in preparation for the next feature to implement

--- backend/prisma/migrations/20250908075326_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "public"."Module" (
-    "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "category" TEXT NOT NULL,
-
-    CONSTRAINT "Module_pkey" PRIMARY KEY ("id")
-);
