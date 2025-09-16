0b554c0147056cfc134561702dbc4b350afec652
Jhon <jbgutang771@gmail.com>
2025-09-16 23:07:46 +0800

    refactored course by id feature and simplifying the structure in a feature based architecture

--- frontend/src/app/courses/[courseTitle]/page.tsx
+++ frontend/src/app/courses/[courseTitle]/page.tsx
@@ -1,6 +1,4 @@
-import CourseByIdContainer from "@/src/features/course-by-id/course-by-id.container";
-
-// src/app/modules/[module-title]/page.tsx
+import CourseById from "@/src/features/course-by-id/CourseById";
 interface CoursebydIdPageProps {
     params: {
       "courseTitle": string;
@@ -10,7 +8,7 @@ interface CoursebydIdPageProps {
   export default async function CourseByIdPage({ params }: CoursebydIdPageProps) {
     const {courseTitle} = await params;
     return (
-        <CourseByIdContainer name={courseTitle}/>
+        <CourseById name={courseTitle}/>
     );
   }
   
---
+++ frontend/src/features/course-by-id/CourseById.tsx
@@ -0,0 +1,46 @@
+"use client";
+import React from "react";
+import { useRedirectLink } from "@/src/shadcn/hooks/useRedirectLink";
+import { useFetchCourse } from "./useFetchCourse";
+import TeacherHomeLayout from "@/src/layout/TeacherHomeLayout";
+import { navItems } from "@/constants/navigationItems";
+import CourseInformation from "./components/CourseInformation";
+import CreateLessonPresentational from "../create-lesson/create-lesson.presentational";
+import ListView from "@/src/shared/ListView";
+import CourseStateGuard from "./components/CourseStateGuard";
+
+interface CourseByIdProps {
+  name: string;
+}
+
+const CourseById: React.FC<CourseByIdProps> = ({ name }) => {
+  const { fromSlug } = useRedirectLink();
+  const { id } = fromSlug(name);
+  const { course, loading, error } = useFetchCourse(id?.toString() || null);
+
+  if (loading || error || !course) {
+    return <CourseStateGuard course={course} loading={loading} error={error} />;
+  }
+
+  return (
+    <TeacherHomeLayout pageTitle={name} navItems={navItems}>
+      <div className="flex h-[91vh]">
+        <CourseInformation course={course} />
+        <div className="h-full py-10 px-4 flex-1 flex flex-col">
+          <div className="flex items-center justify-between mb-4">
+            <div className="text-xl font-semibold">Lessons</div>
+            <CreateLessonPresentational courseId={course.id} />
+          </div>
+          <div
+            className="flex-1 overflow-y-auto"
+            style={{ maxHeight: "calc(91vh - 80px)" }}
+          >
+            <ListView data={course.lessons} />
+          </div>
+        </div>
+      </div>
+    </TeacherHomeLayout>
+  );
+};
+
+export default CourseById;
