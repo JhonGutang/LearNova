f8577fbb2fa1a9652890197f9d125ce0abbf66e1
Jhon <jbgutang771@gmail.com>
2025-09-18 14:51:57 +0800

    implemeneted create or update when creating or modifying existing lesson pages. optimized checking for existing page through adding another method to achieve 0(1) complexity

--- backend/src/features/lesson_page/lesson_page.service.ts
+++ backend/src/features/lesson_page/lesson_page.service.ts
@@ -1,27 +1,47 @@
 import prisma from "../../config/prisma";
-import { CreateLessonPageInput, LessonPage } from "../../generated/graphql";
+import { CreateOrUpdateLessonPageInput, LessonPage } from "../../generated/graphql";
+
 export interface LessonPageServiceInterface {
     getLessonPage(id: number): Promise<LessonPage | null>;
     getLessonPages(lessonId: number): Promise<LessonPage[]>;
-    createLessonPage(input: CreateLessonPageInput): Promise<LessonPage>;
+    getLessonByPageNumber(lessonId: number, pageNumber: number): Promise<LessonPage | null>; 
+    createLessonPage(input: CreateOrUpdateLessonPageInput): Promise<LessonPage>;
+    updateLessonPage(id: number, input: CreateOrUpdateLessonPageInput): Promise<LessonPage>;
 }
 
 
 export class LessonPageService implements LessonPageServiceInterface {
-    async getLessonPage(id: number): Promise<LessonPage | null> {
-        return await prisma.lesson_Page.findUnique({
-            where: { id }
-        });
-    }
-
     async getLessonPages(lessonId: number): Promise<LessonPage[]> {
         return await prisma.lesson_Page.findMany({
             where: { lesson_id: lessonId }
         });
     }
 
-    async createLessonPage(input: CreateLessonPageInput): Promise<LessonPage> {
+    async getLessonPage(id: number): Promise<LessonPage | null> {
+        return await prisma.lesson_Page.findUnique({
+            where: { id }
+        });
+    }
+
+    async getLessonByPageNumber(lessonId: number, pageNumber: number): Promise<LessonPage | null> {
+        return await prisma.lesson_Page.findUnique({
+            where: { lesson_id_page_number: { lesson_id: lessonId, page_number: pageNumber } },
+          });
+    }
+
+    async createLessonPage(input: CreateOrUpdateLessonPageInput): Promise<LessonPage> {
         return await prisma.lesson_Page.create({
+            data: {
+                lesson_id: input.lessonId,
+                page_number: input.pageNumber,
+                content_json: input.contentJson,
+            }
+        });
+    }
+
+    async updateLessonPage(id: number, input: CreateOrUpdateLessonPageInput): Promise<LessonPage> {
+        return await prisma.lesson_Page.update({
+            where: { id },
             data: {
                 lesson_id: input.lessonId,
                 page_number: input.pageNumber,
