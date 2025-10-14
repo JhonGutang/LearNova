4341e65ea511796baabb374cae975704a4039c50
Jhon <jbgutang771@gmail.com>
2025-10-14 17:55:19 +0800

    removing dist folder

--- backend/dist/config/prisma.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/prisma.js
-const prisma_1 = require("../../generated/prisma");
-const prisma = new prisma_1.PrismaClient();
-exports.default = prisma;
