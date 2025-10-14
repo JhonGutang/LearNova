d333f1d5dcdf5d92203653c4ed3e0e2ed6e43e06
Jhon <jbgutang771@gmail.com>
2025-10-14 21:32:09 +0800

    adding trust proxy to ensure the session works in cross origin

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,7 @@ const app = express();
 // You can safely remove this if you only use APIs/GraphQL
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
-
+app.set("trust proxy", 1);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
